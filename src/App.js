ec6834ae5a369b5f396e760a713b702fafdf6444
Robin Ung <Robin_ung@hotmail.com>
2024-12-12 13:28:47 +0000

    Add route to dashboard, budgets and transactions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Navbar from "./components/Navbar";
 import RegisterPage from "./pages/Register";
 import LoginPage from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import Budgets from "./pages/Budgets";
 import UserProfile from "./pages/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -23,10 +26,34 @@ const App = () => {
             <Route path="/login" element={<LoginPage />} />
             {/* Protect the profile route */}
             <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
               path="/profile"
               element={
                 <ProtectedRoute>
                   <UserProfile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/transactions"
+              element={
+                <ProtectedRoute>
+                  <Transactions />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/budgets"
+              element={
+                <ProtectedRoute>
+                  <Budgets />
                 </ProtectedRoute>
               }
             />
