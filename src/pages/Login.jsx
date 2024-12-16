f62ca0f56f24021dc981e3130cc56eded3583481
Robin Ung <Robin_ung@hotmail.com>
2024-12-16 22:18:41 +0000

    Add Welcometext and a explenation to what you can do on the site

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,8 @@ import {
   Col,
   Alert,
 } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useNavigate, useLocation } from "react-router-dom";
 import apiClient from "../api/apiClient";
 import "../styles/pages/LoginPage.css";
 
@@ -17,8 +18,12 @@ const LoginPage = () => {
     username: "",
     password: "",
   });
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const [error, setError] = useState(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showExplanation, setShowExplanation] = useState(true); // State for toggling explanation visibility
+  const location = useLocation();
+  const successMessage = location.state?.successMessage;
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -26,6 +31,10 @@ const LoginPage = () => {
       ...credentials,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible((prevVisible) => !prevVisible);
   };
 
   const handleSubmit = async (e) => {
@@ -58,25 +67,63 @@ const LoginPage = () => {
     }
   };
 
+  const toggleExplanation = () => {
+    setShowExplanation((prevState) => !prevState); // Toggle the visibility of the explanation
+  };
+
   return (
     <Container className="mt-5">
       <Row className="justify-content-center">
-        <Col md={4}>
+        <Col xs="auto">
           <Card className="shadow-sm">
             <Card.Header className="text-center bg-success text-white">
-              <h3>Login</h3>
+              <h1>Welcome to Budget</h1>
+              <Button
+                variant="link"
+                onClick={toggleExplanation}
+                className="mt-2 text-light"
+                disabled={isSubmitting}
+              >
+                {showExplanation ? "Hide Explanation" : "Show Explanation"}
+              </Button>
             </Card.Header>
             <Card.Body>
+              {showExplanation && (
+                <div className="mb-4">
+                  <p>
+                    This platform helps you keep track of your finances!
+                    <br />
+                    By managing your transactions and budgets. <br />
+                    Once you log in <br />
+                    - You can add your expenses and incomes.
+                    <br />
+                    - Create budgets, and gain an overview of your economy.
+                    <br />
+                    Start organizing your finances today!
+                    <br />
+                  </p>
+                </div>
+              )}
+
+              {successMessage && (
+                <Alert variant="success" className="text-center">
+                  {successMessage}
+                </Alert>
+              )}
               {error && (
                 <Alert variant="danger" className="text-center">
                   {error}
                 </Alert>
               )}
+              <p className="text-center mb-4">
+                Please log in to access your account.
+              </p>
               <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3">
-                  <Form.Label>Username</Form.Label>
+                  <Form.Label htmlFor="username">Username</Form.Label>
                   <Form.Control
                     type="text"
+                    id="username"
                     name="username"
                     value={credentials.username}
                     onChange={handleChange}
@@ -87,17 +134,28 @@ const LoginPage = () => {
                   />
                 </Form.Group>
                 <Form.Group className="mb-3">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    name="password"
-                    value={credentials.password}
-                    onChange={handleChange}
-                    placeholder="Enter your password"
-                    autoComplete="current-password"
-                    required
-                    disabled={isSubmitting}
-                  />
+                  <Form.Label htmlFor="password">Password</Form.Label>
+                  <div className="d-flex align-items-center">
+                    <Form.Control
+                      type={passwordVisible ? "text" : "password"}
+                      id="password"
+                      name="password"
+                      value={credentials.password}
+                      onChange={handleChange}
+                      placeholder="Enter your password"
+                      autoComplete="current-password"
+                      required
+                      disabled={isSubmitting}
+                    />
+                    <Button
+                      variant="link"
+                      onClick={togglePasswordVisibility}
+                      className="ms-2"
+                      disabled={isSubmitting}
+                    >
+                      {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+                    </Button>
+                  </div>
                 </Form.Group>
                 <Button
                   variant="success"
